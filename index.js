99cd6d8c9d657a3fac4f01478dc195f7db2cc290
Youssef <youssef.dahbi7@hotmail.com>
2021-07-06 17:15:10 +0200

    javascript page description

--- index.js
+++ index.js
@@ -4,27 +4,62 @@ fetch(create_item_page_url)
   .then(function(item_page_response) {if (item_page_response.ok) {return item_page_response.json(); } })
   .then(function(item_page_json) {
     // -------------------------------------------------------------------
-        let item_name =item_page_json.name;
-        let camera_name = document.getElementById("camera_name");
-        camera_name.innerHTML = item_name;
 
-        let item_imageUrl = item_page_json.imageUrl;
-        let camera_image = document.getElementById("camera_image");
-        camera_image.src = item_imageUrl;
+//div id=camera
+let div_id_camera = document.createElement('div');
+div_id_camera.id = "camera";
 
-        let item_description = item_page_json.description;
-        let camera_description = document.getElementById("camera_description");
-        camera_description.innerHTML = item_description;
+let container = document.getElementById("container");
+container.appendChild(div_id_camera);
+   
+    //div class = camera_choix
+	let div_class_camera_choix = document.createElement('div');
+	div_class_camera_choix.className = "camera_choix";
 
-        let item_price = (item_page_json.price)/100;
-        let item_price_decimal = item_price.toFixed(2);
-        let camera_price = document.getElementById("camera_price");
-        camera_price.innerHTML = item_price_decimal + "€" ;
-        // pareil pour la description
-        // pareil pour le prix
+	let camera = document.getElementById("camera");
+	camera.appendChild(div_class_camera_choix);
+
+	//div class = camera_images
+	let camera_images = document.createElement('div');
+	camera_images.className = "camera_images";
+
+	let camera_choix = document.getElementsByClassName("camera_choix");
+	div_class_camera_choix.appendChild(camera_images);
+
+	//images
+		let item_text_node_image = document.createTextNode(item_page_json.imageUrl);
+		let imgElement = document.createElement("img");
+	    imgElement.id = "camera_image";
+	    imgElement.setAttribute("src", item_page_json.imageUrl);
+	    imgElement.appendChild(item_text_node_image);
+        camera_images.appendChild(imgElement);
+    //div description
+   let div_class_description = document.createElement("div");
+   div_class_description.className = "description";
+   camera.appendChild(div_class_description);
+   			// h2 camera name
+   			let item_text_node_name = document.createTextNode(item_page_json.name); 
+			let new_item_page_name = document.createElement("h2"); 
+			new_item_page_name.appendChild(item_text_node_name);
+			div_class_description.appendChild(new_item_page_name);
 
 
-
+			let div_class_description_titre = document.createElement("h3");
+			let description_node_titre = document.createTextNode("Description")
+   			div_class_description_titre.className = "description_titre";
+   			div_class_description_titre.appendChild(description_node_titre);
+   			div_class_description.appendChild(div_class_description_titre);
+			//paragraphe
+			let item_text_node_description = document.createTextNode(item_page_json.description); 
+			let new_item_page_description = document.createElement("p"); 
+			new_item_page_description.appendChild(item_text_node_description);
+			div_class_description.appendChild(new_item_page_description);
+			//prix
+			let item_text_node_prix = document.createTextNode(item_page_json.price/100+",00"+"€");
+			let item_price_decimal = item_text_node_prix; 
+			let new_item_page_prix = document.createElement("p"); 
+			new_item_page_prix.appendChild(item_price_decimal);
+			div_class_description.appendChild(new_item_page_prix);
     // -------------------------------------------------------------------
      }) //.then(function(item_page_json)
   .catch(function(err) {
