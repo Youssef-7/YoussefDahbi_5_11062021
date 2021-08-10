c99622f5368e705a25cd4ea2748bc6bb4380f9b1
Youssef <youssef.dahbi7@hotmail.com>
2021-08-10 23:02:09 +0200

    css page

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,5 +1,5 @@
         const responseId = localStorage.getItem("OrderId")
-        console.log("Id de la commande")
+        console.log("Test n°1: Id de la commande")
         console.log(responseId)
         let new_item_node_name = document.createTextNode("Bravo votre commande n°"+responseId+" "+"arrivera très prochainement")
         let pElement = document.createElement("p");
