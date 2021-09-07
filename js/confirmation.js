8a917174f258f34934def91d6c198713a9d1f8c7
Youssef-7 <80470335+Youssef-7@users.noreply.github.com>
2021-09-07 19:42:05 +0200

    correction affichage

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,9 +1,11 @@
-        const responseId = localStorage.getItem("OrderId")
-        console.log("Test n°1: Id de la commande")
-        console.log(responseId)
-        let new_item_node_name = document.createTextNode("Bravo votre commande n°"+responseId+" "+"arrivera très prochainement")
-        let pElement = document.createElement("p");
-        pElement.classList = "confirmation";
-        pElement.appendChild(new_item_node_name);
-        let div_id_camera_confirmation = document.getElementById("camera_confirmation");
-        div_id_camera_confirmation .appendChild(pElement)
+const response_id = localStorage.getItem("order_id");
+const response_prix = localStorage.getItem("prixTotal");
+console.log(response_prix)
+console.log("Test n°1: Id de la commande")
+console.log(response_id)
+let new_item_node_name = document.createTextNode("Bravo votre commande n°"+response_id+" "+", d'un montant de "+ " "+response_prix+" "+"arrivera très prochainement")
+let p_element = document.createElement("p");
+p_element.classList = "confirmation";
+p_element.appendChild(new_item_node_name);
+let div_id_camera_confirmation = document.getElementById("camera_confirmation");
+div_id_camera_confirmation .appendChild(p_element)
